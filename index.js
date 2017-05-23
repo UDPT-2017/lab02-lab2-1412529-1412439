9fd00aaeddb5f8c1ada93918a0db851fe92689c5
meo bach <meo bach>
2017-05-23 14:16:35 +0700

    update server

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ var jsonParser = bodyParser.json()
 var urlencodedParser = bodyParser.urlencoded({ extended: false })
 //start
 var port = 1000;
-app.listen(3000);
+app.listen(process.env.PORT||3000);
 app.post('/register',jsonParser,controllers.home.register);
 app.post('/login',jsonParser,controllers.home.login);
 app.post('/add_friend',jsonParser,controllers.users.add_friend);
